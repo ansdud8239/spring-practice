22ca36255b2bcb98b4f505a964e53112538856ab
ansdud8239 <ansdud8239@naver.com>
2023-01-30 15:05:17 +0900

    deplolt plug in

---
+++ guestbook03/src/main/java/com/douzone/guestbook/controller/GuestbookController.java
@@ -0,0 +1,44 @@
+package com.douzone.guestbook.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.douzone.guestbook.repository.GuestbooRepository;
+import com.douzone.guestbook.vo.GuestBookVo;
+
+@Controller
+public class GuestbookController {
+	@Autowired
+	private GuestbooRepository guestbookRepository;
+	
+
+	@RequestMapping("/")
+	public String list(Model model) {				
+		List<GuestBookVo> list = guestbookRepository.findAll();
+		model.addAttribute("list",list);
+		return "/WEB-INF/views/list.jsp";
+	}
+
+	@RequestMapping(value = "/add",method = RequestMethod.POST)
+	public String add(GuestBookVo vo) {	
+		guestbookRepository.insert(vo);		
+		return "redirect:/";
+	}
+	@RequestMapping(value = "/delete",method = RequestMethod.GET)
+	public String delete(String no,Model model) {	
+		model.addAttribute("no",no);
+		return "/WEB-INF/views/deleteform.jsp";
+	}
+	@RequestMapping(value = "/delete",method = RequestMethod.POST)
+	public String delete(int no,String password) {	
+
+		boolean result =guestbookRepository.delete(no, password);
+		return "redirect:/";
+	}
+	
+}
---
+++ guestbook03/src/main/java/com/douzone/guestbook/repository/GuestbooRepository.java
@@ -0,0 +1,135 @@
+package com.douzone.guestbook.repository;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
+import com.douzone.guestbook.vo.GuestBookVo;
+
+@Repository
+public class GuestbooRepository {
+	public void insert(GuestBookVo vo) {
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		try {
+			conn = getConnetion();
+			String sql = "insert into guestbook values(null,?,?,?,now())";
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, vo.getName());
+			pstmt.setString(2, vo.getPassword());
+			pstmt.setString(3, vo.getMessage());
+
+			pstmt.executeUpdate();
+
+		} catch (SQLException e) {
+			System.out.println("error: " + e);
+		} finally {
+			try {
+				if (pstmt != null) {
+					pstmt.close();
+				}
+				if (conn != null) {
+					conn.close();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+
+	}
+
+	public List<GuestBookVo> findAll() {
+		List<GuestBookVo> result = new ArrayList<>();
+
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		try {
+			conn = getConnetion();
+			String sql = "select no,name,message,reg_date from guestbook order by no desc";
+			pstmt = conn.prepareStatement(sql);
+
+			rs = pstmt.executeQuery();
+
+			// 5. 결과
+			while (rs.next()) {
+				GuestBookVo vo = new GuestBookVo();
+				vo.setNo(rs.getLong(1));
+				vo.setName(rs.getString(2));
+				vo.setMessage(rs.getString(3));
+				vo.setRegDate(rs.getString(4));
+				result.add(vo);
+			}
+
+		} catch (SQLException e) {
+			System.out.println("error: " + e);
+		} finally {
+			try {
+				if (rs != null) {
+					rs.close();
+				}
+				if (pstmt != null) {
+					pstmt.close();
+				}
+				if (conn != null) {
+					conn.close();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+
+		return result;
+	}
+
+	public boolean delete(int no,String password) {
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		boolean result = false;
+		try {
+			conn = getConnetion();
+			String sql = "delete from guestbook where no=? and password=?";
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setLong(1, no);
+			pstmt.setString(2, password);
+			result = pstmt.executeUpdate()==1?true:false;
+			
+
+		} catch (SQLException e) {
+			System.out.println("error: " + e);
+		} finally {
+			try {
+				if (pstmt != null) {
+					pstmt.close();
+				}
+				if (conn != null) {
+					conn.close();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		return result;
+	}
+
+	private Connection getConnetion() throws SQLException {
+		Connection conn = null;
+		try {
+			Class.forName("org.mariadb.jdbc.Driver");
+
+			String url = "jdbc:mariadb://192.168.10.109:3307/webdb?charset=utf8";
+			conn = DriverManager.getConnection(url, "webdb", "webdb");
+
+		} catch (ClassNotFoundException e) {
+			System.out.println("드라이버 로딩 실패: " + e);
+		}
+		return conn;
+
+	}
+}
