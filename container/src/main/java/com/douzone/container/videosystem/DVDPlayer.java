87896d13e193b194de8210b44433abb6a2c918fe
ansdud8239 <ansdud8239@naver.com>
2023-02-10 15:33:41 +0900

    Explicit - Java config #2

--- container/src/main/java/com/douzone/container/videosystem/DVDPlayer.java
+++ container/src/main/java/com/douzone/container/videosystem/DVDPlayer.java
@@ -2,11 +2,22 @@ package com.douzone.container.videosystem;
 
 public class DVDPlayer {
 	private DigitalVideoDisc dvd;
-	
+
+	public DVDPlayer() {
+	}
+
 	public DVDPlayer(DigitalVideoDisc dvd) {
 		this.dvd = dvd;
 	}
-	
+
+	public DigitalVideoDisc getDvd() {
+		return dvd;
+	}
+
+	public void setDvd(DigitalVideoDisc dvd) {
+		this.dvd = dvd;
+	}
+
 	public String play() {
 		return dvd.play();
 	}
